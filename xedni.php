b29a43ce349efc34b7065f2f5782b2912e0a2dbc
amisHost <102328522+uwuamishost@users.noreply.github.com>
2022-06-11 18:16:42 +0430

    Add files via upload

---
+++ xedni.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+
+    <!-- -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css"  rel="stylesheet" href="elyts.css">
+    <title> Wrong One, Dude </title>
+</head>
+<body>
+<!-- BANNER -->
+
+<style>@import url('https://v1.fontapi.ir/css/Vazir:400');</style>
+
+    <section id="banner">
+        <div class="watermark">
+            <h4>Photo captured in IRAN by <a href="https://unsplash.com/@sina_drakhshani"> 🔗sina drakhshani </a> </h4>
+
+        </div>
+        <div class="bannertxt">
+            <p> وِلکام </p>
+            <h4 class="pepega">⠄⠄⠄⠄⠄⠄⠄⢀⣠⣶⣾⣿⣶⣦⣤⣀⠄⢀⣀⣤⣤⣤⣤⣄⠄⠄⠄⠄⠄⠄<br>
+⠄⠄⠄⠄⠄⢀⣴⣿⣿⣿⡿⠿⠿⠿⠿⢿⣷⡹⣿⣿⣿⣿⣿⣿⣷⠄⠄⠄⠄⠄<br>
+⠄⠄⠄⠄⠄⣾⣿⣿⣿⣯⣵⣾⣿⣿⡶⠦⠭⢁⠩⢭⣭⣵⣶⣶⡬⣄⣀⡀⠄⠄<br>
+⠄⠄⠄⡀⠘⠻⣿⣿⣿⣿⡿⠟⠩⠶⠚⠻⠟⠳⢶⣮⢫⣥⠶⠒⠒⠒⠒⠆⠐⠒<br>
+⠄⢠⣾⢇⣿⣿⣶⣦⢠⠰⡕⢤⠆⠄⠰⢠⢠⠄⠰⢠⠠⠄⡀⠄⢊⢯⠄⡅⠂⠄<br>
+⢠⣿⣿⣿⣿⣿⣿⣿⣏⠘⢼⠬⠆⠄⢘⠨⢐⠄⢘⠈⣼⡄⠄⠄⡢⡲⠄⠂⠠⠄<br>
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣥⣀⡁⠄⠘⠘⠘⢀⣠⣾⣿⢿⣦⣁⠙⠃⠄⠃⠐⣀<br>
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣋⣵⣾⣿⣿⣿⣿⣦⣀⣶⣾⣿⣿⡉⠉⠉<br>
+⣿⣿⣿⣿⣿⣿⣿⠟⣫⣥⣬⣭⣛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄<br>
+⣿⣿⣿⣿⣿⣿⣿⠸⣿⣏⣙⠿⣿⣿⣶⣦⣍⣙⠿⠿⠿⠿⠿⠿⠿⠿⣛⣩⣶⠄<br>
+⣛⣛⣛⠿⠿⣿⣿⣿⣮⣙⠿⢿⣶⣶⣭⣭⣛⣛⣛⣛⠛⠛⠻⣛⣛⣛⣛⣋⠁⢀<br>
+⣿⣿⣿⣿⣿⣶⣬⢙⡻⠿⠿⣷⣤⣝⣛⣛⣛⣛⣛⣛⣛⣛⠛⠛⣛⣛⠛⣡⣴⣿<br>
+⣛⣛⠛⠛⠛⣛⡑⡿⢻⢻⠲⢆⢹⣿⣿⣿⣿⣿⣿⠿⠿⠟⡴⢻⢋⠻⣟⠈⠿⠿<br>
+⣿⡿⡿⣿⢷⢤⠄⡔⡘⣃⢃⢰⡦⡤⡤⢤⢤⢤⠒⠞⠳⢸⠃⡆⢸⠄⠟⠸⠛⢿<br>
+⡟⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸</h4>
+        </div>
+        <a href="#about">
+            <div class="scrolldown"> </div>
+        </a>
+    </section>
+
+
+<!-- SIDE.NAV -->
+
+
+    <div id="sidenav">
+        <nav>
+            <ul>
+                <li> <a href="#"> Home </a> </li>
+                <li> <a href="#third"> Contact me </a> </li>
+            </ul>
+        </nav>
+    </div>
+
+    <div id="menubtn" >
+        <img src="otohp/3x.gif" id="menu">
+    </div>
+
+    <script>
+        var dokme = document.getElementById("menubtn")
+        var icon = document.getElementById("menu")
+        var menu = document.getElementById("sidenav")
+
+        dokme.onclick = function(){
+            if(menu.style.right == "-220px"){
+                menu.style.right = "20px"
+                menu.style.boxShadow= "10px 10px 50px 8px #fff"
+            }
+            else{
+                menu.style.right = "-220px"
+                menu.style.boxShadow= "None"
+            }
+        }
+    </script>
+
+
+<!-- ABOUT -->
+
+
+    <section id="about">
+        <div class="watermark">
+            <h3>Photo captured in IRAN by <a href="https://unsplash.com/@heinrich_boll"> 🔗Mohammad Rezaie</a> </h3>
+        </div>
+        <h4> ABOUT ME </h4>
+        <img src="otohp/pfp.jpg" alt="" class="pfp">
+        <h5><b> I'm amis , a </b> </h5>
+        <p>
+            Hey, my name is amis <br>
+                born in <i> 2005 JULY </i> ,<b> IRAN </b> <br>
+                junior developer ,web developer & software engineer (in basic level) <br>
+                professional esport player, <br>
+                and Very beginner musician 
+        </p>
+        <a href="#third">
+            <div class="scrolldown"> </div>
+        </a>
+    </section>
+
+<!-- SOCIAL_LINKS -->
+    <section id="third">
+        <div class="watermark">
+            <h4>Photo captured in IRAN by <a href="https://unsplash.com/@davoodjalali"> 🔗davood jalali </a> </h4>
+        </div>
+        <div class="mains">
+            <a href="https://instagram.com/_uwu_corpse">    <img src="otohp/instagram.png" alt="instagram" class="imgs">    </a>
+            <a href="https://twitter.com/uwu_amis">    <img src="otohp/twitter.png" alt="twitter" class="imgs">    </a>
+            <a href="mailto:amis.privacy@gmail.com">    <img src="otohp/email.png" alt="email" class="imgs">    </a>
+            <a href="https://t.me/uwuamis">    <img src="otohp/telegram.png" alt="telegram" class="imgs">    </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="otohp/up-arrow-png-27176.png" alt="" class="arrowup">
+            </a>
+        </div>
+    </section>
+</body>
+</html>
